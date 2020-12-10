32d9a7a2ffca8dac599bfe3c66d0b0221428e961
Augusto Lima <augustocesar827@gmail.com>
2020-12-10 11:18:08 -0300

    Initial structure missing files

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports =  {
+  parser: '@typescript-eslint/parser',
+
+  extends:  [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  
+  rules: {
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+  },
+};
