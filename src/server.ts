f6c3f4d58dae3ef755fdf0f7102beed12a65c13a
Augusto Lima <augustocesar827@gmail.com>
2021-01-28 19:13:03 -0300

    Fix in database connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import Env from './env';
 import app from './app';
-import './database';
+import database from './database';
 
 const port = Env.get('PORT', 8080);
 const url = Env.get('URL', `http://localhost:${port}`);
 
-app.listen(port, () => {
-  console.log(`Server running on ${url}`);
-});
+database
+  .connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on ${url}`);
+    });
+  })
+  .catch(() => {
+    console.log('Server disconnected. Please verify the database');
+    process.exit(0);
+  });
