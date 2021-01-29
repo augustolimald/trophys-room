cbb2aa951f8453218bc663e4cb6c5e3b0268dcbb
Augusto Lima <augustocesarld@hotmail.com>
2021-01-29 10:55:00 -0300

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -9,7 +9,7 @@ const connection =
       }
     : {
         type: 'postgres',
-        url: `${process.env.DATABASE_URL}?ssl=true`,
+        url: `${process.env.DATABASE_URL}?sslmode=require`,
       };
 
 const entities = [];
