c6434419572bcc50a873eab9ae8cc37aa8e72771
Augusto Lima <augustocesar827@gmail.com>
2021-01-30 14:33:46 -0300

    Initial structure to Frontend

---
+++ src/public/cadastro/index.js
@@ -0,0 +1,8 @@
+/**
+ * const formData = new FormData();
+  formData.append('profile_picture', document.getElementById('inputFile').files[0]);
+ */
+
+function cadastrar() {
+  window.location.href = '..';
+}
